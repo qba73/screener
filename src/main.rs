439a54a011e2981155c88969ff65a2f9aebb7319
Jakub Jarosz <jakub@jarosz.dev>
2024-12-17 20:54:08 +0000

    Hardcoded 1st scan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use serde_sarif::sarif::Sarif;
+
+fn main() {    
+    let data = fs::read_to_string("tests/data/sample.sarif").unwrap();
+    let sarif: Sarif = serde_json::from_str(&data).unwrap();
+    
+    for run in sarif.runs  {
+        for result in run.results.unwrap() {
+            println!("{}, {}", result.level.unwrap(), result.message.text.unwrap())
+        }
+    }
 }

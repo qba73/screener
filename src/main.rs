0d51613673e3366ed0b6e3efac5078b012a8d1be
Jakub Jarosz <jakub@jarosz.dev>
2024-12-22 07:32:04 +0000

    Parse sarif report from the command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use std::fs;
+use std::{
+    env,
+    error::Error,
+    fs
+};
+
 use serde_sarif::sarif::Sarif;
 
-fn main() {    
-    let data = fs::read_to_string("tests/data/sample.sarif").unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage:");
+        println!("screener: <sarif-report.json>");
+        return Ok(());
+    }
+
+    let data = fs::read_to_string(&args[1]).unwrap();
     let sarif: Sarif = serde_json::from_str(&data).unwrap();
     
     for run in sarif.runs  {
@@ -10,4 +22,6 @@ fn main() {
             println!("{}, {}", result.level.unwrap(), result.message.text.unwrap())
         }
     }
+
+    Ok(())
 }

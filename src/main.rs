93712ef64e8e63dc0f607c3f0c510342c621f9fb
Jakub Jarosz <jakub@jarosz.dev>
2025-01-10 15:44:57 +0000

    Testing options to filter CVEs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    env,
-    error::Error,
-    fs
-};
+use std::{collections::HashMap, env, error::Error, fs};
 
 use serde_sarif::sarif::Sarif;
 
@@ -16,11 +12,67 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let data = fs::read_to_string(&args[1]).unwrap();
     let sarif: Sarif = serde_json::from_str(&data).unwrap();
-    
-    for run in sarif.runs  {
-        for result in run.results.unwrap() {
-            println!("{}, {}", result.level.unwrap(), result.message.text.unwrap())
+
+    for run in sarif.runs {
+        let mut cve = HashMap::new();
+
+        for rule in run.tool.driver.rules.unwrap() {
+            cve.insert(
+                rule.id,
+                rule.properties
+                    .unwrap()
+                    .additional_properties
+                    .get("cvssV3_severity")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string(),
+            );
         }
+
+        //println!("{cve:?}");
+
+        // for r in run.results.unwrap().iter().filter(|r|{ 
+        //     let id = &r.rule_id.clone().unwrap();
+        //     let level = cve.get(id).unwrap();
+
+        //     level == "HIGH"
+        // }) {
+        //     println!("CVE: {:?}", r.rule_id.as_ref().unwrap());
+        // };
+
+        // implement filter map here:
+        for id in run.results.unwrap().into_iter().filter_map (|r|{ 
+            let id = &r.rule_id.clone().unwrap();
+            let level = cve.get(id).unwrap();
+
+            if level == "HIGH" {
+                Some(id.clone())
+            } else {
+                None
+            }
+        }) {
+            println!("CVE: {:?}", id);
+        };
+
+
+
+
+        // for r in run.results.unwrap() {
+        //     let id = r.rule_id.unwrap();
+        //     let level = cve.get(&id).unwrap();
+            
+        //     if level == "HIGH" {
+        //         println!("Level: {}, CVE: {}", level, &id);
+        //     }
+           
+
+             
+        //     // if r.level.unwrap().as_str().unwrap() == "HIGH" {
+        //     //     println!("HIGH CVE: {}", r.message.text.unwrap())
+        //     // }
+
+        // }
     }
 
     Ok(())

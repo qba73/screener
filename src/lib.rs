01e60931b400a3e2942c81fe548b835b7400e3e0
Jakub Jarosz <jakub@jarosz.dev>
2025-04-26 10:09:50 +0100

    move logic to a separate func
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,60 @@
-use std::str;
+use std::{
+    collections::HashMap, fs, str::{self}
+};
 
-pub fn parse_cves(path: &str) -> Vec<String> {
-    Vec::new()
+use anyhow::{Ok, Result};
+
+use serde_json;
+use serde_sarif::sarif::Sarif;
+
+
+pub fn parse_cves(path: &str) -> Result<Vec<String>> {
+    let mut cves = Vec::new();
 
     // add business logic
+    let data = fs::read_to_string(path).unwrap();
+    let sarif: Sarif = serde_json::from_str(&data).unwrap();
+
+    for run in sarif.runs {
+        let mut cve = HashMap::new();
+
+        for rule in run.tool.driver.rules.unwrap() {
+            cve.insert(
+                rule.id,
+                rule.properties
+                    .unwrap()
+                    .additional_properties
+                    .get("cvssV3_severity") // causes panic here - no key in the file
+                    .unwrap_or(&serde_json::Value::String(String::from("HIGH")))
+                    .as_str()
+                    .unwrap()
+                    .to_string(),
+            );
+        }
+
+        // implement filter map here:
+        for id in run.results.unwrap().into_iter().filter_map (|r|{
+            let id = &r.rule_id.clone().unwrap();
+            let level = cve.get(id).unwrap();
+
+            if level == "HIGH" {
+                Some(id.clone())
+            } else {
+                None
+            }
+        }) {
+            cves.push(id);
+        };
+
+    }
+
+    Ok(cves)
 }
+
+
 
 #[test]
 fn parse_cves_returns_high_serverities_cves_from_sarif_file() {
-    let cves = parse_cves("tests/data/debian.sarif.json");
+    let cves = parse_cves("tests/data/debian.sarif.json").unwrap();
     assert_eq!(cves, vec!["CVE-2024-45338"]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,12 @@
-use std::{
-    collections::HashMap,
-    env::{self, args},
-    error::Error,
-    fs,
-};
-
-use serde_sarif::sarif::Sarif;
-
 use anyhow::{Ok, Result};
-
+use std::env::args;
 use screener::parse_cves;
 
 fn main() -> Result<()> {
     let path = args().skip(1).next().unwrap();
-    let cves = parse_cves(&path);
+    let cves = parse_cves(&path)?;
     for cve in cves {
         println!("{cve}")
     }
     Ok(())
 }
-
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let args: Vec<String> = env::args().collect();
-//     if args.len() != 2 {
-//         println!("Usage:");
-//         println!("screener: <sarif-report.json>");
-//         return Ok(());
-//     }
-
-//     let data = fs::read_to_string(&args[1]).unwrap();
-//     let sarif: Sarif = serde_json::from_str(&data).unwrap();
-
-//     for run in sarif.runs {
-//         let mut cve = HashMap::new();
-
-//         for rule in run.tool.driver.rules.unwrap() {
-//             cve.insert(
-//                 rule.id,
-//                 rule.properties
-//                     .unwrap()
-//                     .additional_properties
-//                     .get("cvssV3_severity")
-//                     .unwrap()
-//                     .as_str()
-//                     .unwrap()
-//                     .to_string(),
-//             );
-//         }
-
-//         //println!("{cve:?}");
-
-//         // for r in run.results.unwrap().iter().filter(|r|{
-//         //     let id = &r.rule_id.clone().unwrap();
-//         //     let level = cve.get(id).unwrap();
-
-//         //     level == "HIGH"
-//         // }) {
-//         //     println!("CVE: {:?}", r.rule_id.as_ref().unwrap());
-//         // };
-
-//         // implement filter map here:
-//         for id in run.results.unwrap().into_iter().filter_map (|r|{
-//             let id = &r.rule_id.clone().unwrap();
-//             let level = cve.get(id).unwrap();
-
-//             if level == "HIGH" {
-//                 Some(id.clone())
-//             } else {
-//                 None
-//             }
-//         }) {
-//             println!("CVE: {:?}", id);
-//         };
-
-//         // for r in run.results.unwrap() {
-//         //     let id = r.rule_id.unwrap();
-//         //     let level = cve.get(&id).unwrap();
-
-//         //     if level == "HIGH" {
-//         //         println!("Level: {}, CVE: {}", level, &id);
-//         //     }
-
-//         //     // if r.level.unwrap().as_str().unwrap() == "HIGH" {
-//         //     //     println!("HIGH CVE: {}", r.message.text.unwrap())
-//         //     // }
-
-//         // }
-//     }
-
-//     Ok(())
-// }
